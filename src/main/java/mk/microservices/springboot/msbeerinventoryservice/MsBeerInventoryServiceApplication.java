150494e631e837e7225f608b828cc02981334d78
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-17 14:25:25 +0200

    Adds the initial project

---
+++ src/main/java/mk/microservices/springboot/msbeerinventoryservice/MsBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package mk.microservices.springboot.msbeerinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBeerInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/microservices/springboot/msbeerinventoryservice/MsBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.microservices.springboot.msbeerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
