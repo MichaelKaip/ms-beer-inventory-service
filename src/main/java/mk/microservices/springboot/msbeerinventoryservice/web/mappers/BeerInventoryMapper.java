74960b6aff742c88c55fe1d1d42ebd7c080f0042
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-20 15:05:10 +0200

    Updates the project to inherit dependencies from BOM. Closes #4

--- src/main/java/mk/microservices/springboot/msbeerinventoryservice/web/mappers/BeerInventoryMapper.java
+++ src/main/java/mk/microservices/springboot/msbeerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -3,6 +3,7 @@ package mk.microservices.springboot.msbeerinventoryservice.web.mappers;
 import mk.microservices.springboot.msbeerinventoryservice.domain.BeerInventory;
 import mk.microservices.springboot.msbeerinventoryservice.web.model.BeerInventoryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 public interface BeerInventoryMapper {
