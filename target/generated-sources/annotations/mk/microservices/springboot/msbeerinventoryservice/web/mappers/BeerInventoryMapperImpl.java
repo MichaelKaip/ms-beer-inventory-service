e52d8aa9f7aaef9d245256ccb1663caf704fa64c
Michael Kaip <michael.kaip@student.htw-berlin.de>
2020-08-20 18:17:52 +0200

    Changes reference in parent to the newly released BOM and adds the 'https://jitpack.io' repository.

--- target/generated-sources/annotations/mk/microservices/springboot/msbeerinventoryservice/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/mk/microservices/springboot/msbeerinventoryservice/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-20T15:04:06+0200",
+    date = "2020-08-20T16:35:35+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (Oracle Corporation)"
 )
 @Component
